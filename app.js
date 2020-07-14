cf7b479c4c7c81ba6161d7e4846d7768e1b654b8
rashi-sahu <rashi.sahu13@gmail.com>
2020-07-15 00:03:33 +0530

    Fetch unique brand name from make up api

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const request = require("request");
+const R = require("ramda");
 
-app.get('/ping', (req, res) => res.send({message : 'pong'}))
-app.get('/pong', (req, res) => res.send({message : 'ping'}))
+var url = "http://makeup-api.herokuapp.com/api/v1/products.json";
 
-app.listen(process.env.PORT || 3000, () => console.log(`Example app listening at http://localhost:${process.env.PORT || 3000}`))
+var getUniqueBrandNames = function (body) {
+  var products = JSON.parse(body);
+  var uniqueBrandList = [];
+  products.forEach((element) => {
+    if (element.brand) {
+      uniqueBrandList.push(element.brand);
+    }
+  });
+  return R.uniq(uniqueBrandList);
+};
 
+app.get("/ping", (req, res) => res.send({ message: "pong" }));
+app.get("/pong", (req, res) => res.send({ message: "ping" }));
+
+app.get(
+  "/brands",
+  (req, res) => {
+    request(url, function (error, response, body) {
+      res.send(getUniqueBrandNames(body));
+    });
+  }
+);
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `Example app listening at http://localhost:${process.env.PORT || 3000}`
+  )
+);
