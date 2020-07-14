631bc1d48f8ddf13cfad5f54b15bb7760b69bf71
rashi-sahu <rashi.sahu13@gmail.com>
2020-07-14 22:13:01 +0530

    Added get request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/ping', (req, res) => res.send({message : 'pong'}))
+app.get('/pong', (req, res) => res.send({message : 'ping'}))
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening at http://localhost:${process.env.PORT || 3000}`))
 
